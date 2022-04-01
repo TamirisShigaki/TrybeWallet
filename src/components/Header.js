bd12d6a804b7bf35bbb41e766cbc3de97ce78244
Tamiris Shigaki <shigakiendo@gmail.com>
2022-03-31 23:41:58 -0300

    um pouco de css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './Header.css';
 
 class Header extends React.Component {
   render() {
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { actionUser } from '../actions';
+import './Login.css';
 
 class Login extends React.Component {
   constructor() {
